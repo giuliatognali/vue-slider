03afb9860558003c50bc023881d5b8b40bed2dd1
giuliatognali <giuliatognali@gmail.com>
2023-03-14 18:36:24 +0100

    all thumb active

--- js/script.js
+++ js/script.js
@@ -9,26 +9,31 @@ createApp({
                     image: 'img/01.webp',
                     title: 'Marvel\'s Spiderman Miles Morale',
                     text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
+                    alt: 'Spiderman',
                 },
                 {
                     image: 'img/02.webp',
                     title: 'Ratchet & Clank: Rift Apart',
                     text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
+                    alt: 'Ratchet & Clank',
                 },
                 {
                     image: 'img/03.webp',
                     title: 'Fortnite',
                     text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
+                    alt: 'Fortnite',
                 },
                 {
                     image: 'img/04.webp',
                     title: 'Stray',
                     text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
+                    alt: 'Stray',
                 },
                 {
                     image: 'img/05.webp',
                     title: "Marvel's Avengers",
                     text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
+                    alt: 'Avengers',
                 }
             ]
 
@@ -38,18 +43,23 @@ createApp({
     methods: {
         nextSlide() {
             this.activeSlide++;
-            console.log('next slide')
+            //console.log('next slide')
             if (this.activeSlide > this.slides.length - 1) {
                 this.activeSlide = 0;
             }
         },
         prevSlide() {
             this.activeSlide--;
-            console.log('prev slide')
+            //console.log('prev slide')
             if (this.activeSlide < 0) {
                 this.activeSlide = this.slides.length - 1;
             }
-        }
+        },
+        selectedSlide(index){
+            if (index === this.activeSlide)
+            console.log(index)
+            return true;
+          }
     }
     }).mount('#app');
 
